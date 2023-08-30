516157dbf6076cdb136a8b0a5209a2fa7296655d
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 21:39:28 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_lb" "main" {
-  name = "${var.name}-${var.env}-lb"
-  internal = var.internal
+  name               = "${var.name}-${var.env}-lb"
+  internal           = var.internal
   load_balancer_type = var.load_balancer_type
-  security_groups = [aws_security_group.main.id]
-  subnets  = var.subnets
-  tags = merge({ Name = "${var.name}-${var.env}-lb"}, var.tags)
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+  tags               = merge({ Name = "${var.name}-${var.env}-lb"}, var.tags)
 }
