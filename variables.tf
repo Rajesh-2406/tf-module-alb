6d060753aa003d1d001b9e50545118edd73b8519
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 21:47:47 +0530

    vpc ceration

--- variables.tf
+++ variables.tf
@@ -4,7 +4,9 @@ variable "component" {}
 variable "subnets" {}
 variable "name" {}
 variable "vpc_id" {}
-variable "port" {}
+variable "port" {
+  default = 80
+}
 variable "sg_subnet_cidr" {}
 variable "load_balancer_type" {}
 variable "internal" {}
